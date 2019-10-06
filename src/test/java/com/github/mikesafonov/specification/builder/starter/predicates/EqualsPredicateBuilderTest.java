262c0b7cb7741a26115a576fdcca4ef949ec9d5a
msafonov <msafonovmail@gmail.com>
2019-10-06 19:42:46 +0300

    test: added tests for EqualsPredicateBuilder

---
+++ src/test/java/com/github/mikesafonov/specification/builder/starter/predicates/EqualsPredicateBuilderTest.java
@@ -0,0 +1,23 @@
+package com.github.mikesafonov.specification.builder.starter.predicates;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class EqualsPredicateBuilderTest {
+    @Test
+    void shouldCallEqualsOnExpression(){
+        CriteriaBuilder cb = mock(CriteriaBuilder.class);
+        Object value = "Some value";
+        Expression expression = mock(Expression.class);
+        EqualsPredicateBuilder builder = new EqualsPredicateBuilder(cb, value);
+
+        builder.build(expression);
+
+        verify(cb).equal(expression, value);
+    }
+}
