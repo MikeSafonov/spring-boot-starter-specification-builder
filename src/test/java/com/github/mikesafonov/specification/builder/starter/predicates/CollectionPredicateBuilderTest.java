30c72d414599592b08816fe15750a5715cd11110
msafonov <msafonovmail@gmail.com>
2019-10-06 19:40:34 +0300

    test: added tests for CollectionPredicateBuilder

---
+++ src/test/java/com/github/mikesafonov/specification/builder/starter/predicates/CollectionPredicateBuilderTest.java
@@ -0,0 +1,27 @@
+package com.github.mikesafonov.specification.builder.starter.predicates;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.criteria.Expression;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class CollectionPredicateBuilderTest {
+    @Test
+    void shouldCallInOnExpression() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Some string");
+        CollectionPredicateBuilder builder = new CollectionPredicateBuilder(stringList);
+
+        Expression expression = mock(Expression.class);
+
+        builder.build(expression);
+
+        Object[] objects = stringList.toArray(new Object[0]);
+
+        verify(expression).in(objects);
+    }
+}
