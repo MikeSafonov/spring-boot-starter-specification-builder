8f05b0bb5bc8471c640a5730f02bc73126d61a9f
msafonov <msafonovmail@gmail.com>
2020-05-18 09:08:35 +0300

    feat: created method for building array of attributes expressions for given attribute names

--- src/main/java/com/github/mikesafonov/specification/builder/starter/ExpressionBuilder.java
+++ src/main/java/com/github/mikesafonov/specification/builder/starter/ExpressionBuilder.java
@@ -4,6 +4,7 @@ import com.github.mikesafonov.specification.builder.starter.annotations.Join;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.From;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.lang.reflect.Field;
@@ -18,7 +19,7 @@ public class ExpressionBuilder {
      * @param field        field for expression
      * @param restrictions on restrictions
      * @param <E>          entity type
-     * @return return attribute expression from root or joined attribute expression via {@link Join}
+     * @return attribute expression from root or joined attribute expression via {@link Join}
      */
     @NonNull
     public <E> Expression getExpression(@NonNull Root<E> root, @NonNull FieldWithValue field,
@@ -32,21 +33,67 @@ public class ExpressionBuilder {
      * @param attributeName attribute name
      * @param restrictions  on restrictions
      * @param <E>           entity type
-     * @return return attribute expression from root or joined attribute expression via {@link Join}
+     * @return attribute expression from root or joined attribute expression via {@link Join}
      */
     @NonNull
     public <E> Expression getExpression(@NonNull Root<E> root, @NonNull Field field,
                                         @NonNull String attributeName, @NonNull Predicate... restrictions) {
-        if (field.isAnnotationPresent(Join.class)) {
-            Join[] joins = field.getAnnotationsByType(Join.class);
-            javax.persistence.criteria.Join<Object, Object> join = root.join(joins[0].value(), joins[0].type()).on(restrictions);
-            for (int i = 1; i < joins.length; i++) {
-                join = join.join(joins[i].value(), joins[i].type());
-            }
-            return join.get(attributeName);
-        } else {
-            return root.get(attributeName);
-        }
+        return getFrom(root, field, restrictions).get(attributeName);
     }
 
+    /**
+     * @param root           entity root
+     * @param field          field for expression
+     * @param attributeNames array of attributes names
+     * @param <E>            entity type
+     * @return array of attributes expressions for given attribute names
+     */
+    @NonNull
+    public <E> Expression[] getExpressions(Root<E> root, FieldWithValue field, String[] attributeNames) {
+        Expression[] expressions = new Expression[attributeNames.length];
+        From<?, ?> from = getFrom(root, field.getField());
+        for (int i = 0; i < attributeNames.length; i++) {
+            expressions[i] = from.get(attributeNames[i]);
+        }
+        return expressions;
+    }
+
+    /**
+     * Detects {@link From}. Returns root joined with field if annotation {@link Join} presents and root otherwise.
+     *
+     * @param root         entity root
+     * @param field        field for expression
+     * @param restrictions on restrictions
+     * @param <E>          entity type
+     * @return {@link From} for building expression
+     */
+    private <E> From<?, ?> getFrom(@NonNull Root<E> root,
+                                   @NonNull Field field,
+                                   @NonNull Predicate... restrictions) {
+        if (field.isAnnotationPresent(Join.class)) {
+            return getJoin(root, field, restrictions);
+        }
+        return root;
+    }
+
+    /**
+     * Creates {@link javax.persistence.criteria.Join} of root with field
+     *
+     * @param root         entity root
+     * @param field        field for expression
+     * @param restrictions on restrictions
+     * @param <E>          entity type
+     * @return root joined with field
+     */
+    @NonNull
+    private <E> javax.persistence.criteria.Join<?, ?> getJoin(@NonNull Root<E> root,
+                                                              @NonNull Field field,
+                                                              @NonNull Predicate... restrictions) {
+        Join[] joins = field.getAnnotationsByType(Join.class);
+        javax.persistence.criteria.Join<?, ?> join = root.join(joins[0].value(), joins[0].type()).on(restrictions);
+        for (int i = 1; i < joins.length; i++) {
+            join = join.join(joins[i].value(), joins[i].type());
+        }
+        return join;
+    }
 }
